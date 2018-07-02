b33533e3e7c9e18d295d703e1b0fee97cb4cbb37
cleopasrotich <cleopasrotich81@gmail.com>
2018-07-02 16:12:33 +0300

    quiz: elif statements

---
+++ myex30.py
@@ -0,0 +1,14 @@
+balance = -1
+checkBalance = True
+isActive = True
+
+if not(checkBalance):
+    print "Thank you.Have a nice day!"
+elif isActive and balance > 0:
+    print "Your balance is $", balance
+elif not(isActive):
+    print "Your account is no longer active."
+elif balance == 0:
+    print "Your account is empty."
+else:
+    print "Your balance is negative.\nPlease contact bank."
