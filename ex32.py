970891430b3ec2b6b81f4a3453d286b419b610aa
cleopasrotich <cleopasrotich81@gmail.com>
2018-07-09 17:03:46 +0300

    made some changes on indentation

--- ex32.py
+++ ex32.py
@@ -2,7 +2,7 @@ the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quaters']
 
-#this first kind of for loop goes through a list
+# this first kind of for loop goes through a list
 
 for number in the_count:
     print "This is count %d" % number
@@ -10,26 +10,24 @@ for number in the_count:
 for fruit in fruits:
     print "A fruit of type: %s" % fruit
 
-#we can also go through mixed lists too
-#note that we have to us %r because it's mixed
+# we can also go through mixed lists too
+# note that we have to us %r because it's mixed
 
 for i in change:
     print "I got %r" % i
 
-#we can also build lists
+# we can also build lists
 
 elements = []
 
-#using now the range funtion
+# using now the range funtion
 
 for i in range(0, 6):
     print "Adding %d to list." % i
 
     elements.append(i)
-    
-    #append is a funtion that the list understands
+
+    # append is a funtion that the list understands
 
 for i in elements:
     print "Elements was: %d" % i
-
-
